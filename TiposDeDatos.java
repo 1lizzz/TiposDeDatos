e42a17c071c7935da11a05e2764763a960bebb5e
1lizzz <142031712+1lizzz@users.noreply.github.com>
2023-08-15 23:21:34 +0200

    Add files via upload

---
+++ TiposDeDatos.java
@@ -0,0 +1,20 @@
+
+/* @author Corrales*/
+public class TiposDeDatos {
+    public static void main(String[] args) {
+        //declarar variable en java
+        int numero1 = 10;
+        int numero2 = 20;
+        //proceso
+        int resultado = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int divicion = numero1/numero2;
+        int multiplicar = numero1*numero2;
+        
+        //muetro el resultado
+        System.out.println("la suma es: "+ resultado);
+        System.out.println(",la resta es: "+ resta);
+         System.out.println(",la divicion es: "+ divicion);
+         System.out.println(",la multilicación es: "+ multiplicar);
+    }
+}
